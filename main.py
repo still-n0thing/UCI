fb5ddcc933e0a7be3f32f90d607d8a7c12608087
Dhaval Kumar <dhavalkumar741@gmail.com>
2022-02-04 00:00:36 +0530

    added some things push

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from mimetypes import common_types
 from flask import Flask, render_template, request, redirect, abort
 from models import db, Complaints
 
@@ -13,7 +14,38 @@ def create_table():
 
 @app.route('/', methods = ["GET", "POST"])
 def create_complaints():
-    if request.method == "GET":
-        return render_template('base.html')
+    
+    # Debug Section
+    # lst = employees = Complaints.query.all()
+    # print(lst)
+    # Debug Section
 
+    if request.method == "GET":
+        return render_template('front-page.html')
+
+    if request.method == "POST":
+        aadhar_id = request.form['aadhar_id']
+        name = request.form['name']
+        phoneno = request.form['phoneno']
+        address = request.form['address']
+        postal_code = request.form['postal_code']
+        complaint_type = request.form['complaint_type']
+        complaint_text =  request.form['complaint_text']
+        complaint = Complaints(
+            aadhar_id=aadhar_id, name=name, phoneno=phoneno, 
+            address=address, postal_code=postal_code,
+            complaint_type=complaint_type, complaint_text=complaint_text
+        )
+        # print(complaint)
+        db.session.add(complaint)
+        db.session.commit()
+        return render_template('front-page.html')
+
+
+
+# For Heroku
+def getApp():
+    return app
+
+# Need to be configured before hosting
 app.run(host='localhost', port=5000)
--- models.py
+++ models.py
@@ -1,15 +1,52 @@
+from enum import unique
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import true
 
 db = SQLAlchemy()
 
 class Complaints(db.Model):
     __tablename__ = "COMPLAINTS"
 
-    complaint_id = db.Column(db.Integer, primary_key = True)
-    aadhar_id = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key = True)
+    aadhar_id = db.Column(db.Integer, unique = True)
+    name = db.Column(db.String())
+    phoneno = db.Column(db.Integer())
     address = db.Column(db.String())
     postal_code = db.Column(db.Integer)
     complaint_type = db.Column(db.String())
     complaint_text =  db.Column(db.String(2048))
 
+    def __init__(self, aadhar_id, name, phoneno, address, postal_code, complaint_type, complaint_text) -> None:
+        self.aadhar_id = aadhar_id
+        self.name = name
+        self.phoneno = phoneno
+        self.address = address
+        self.postal_code = postal_code
+        self.complaint_type = complaint_type
+        self.complaint_text = complaint_text
+    
+    def __repr__(self) -> str:
+        return f"({self.aadhar_id}, {self.name}, {self.phoneno}, {self.address}, {self.postal_code}, {self.complaint_type}, {self.complaint_text})"
 
+# TODO: Implement all the tables with complaint_id as Foreign Key
+
+# class Transportation(db.Model):
+#     pass
+
+# class Agriculture(db.Model):
+#     pass
+
+# class DrinkingWater(db.Model):
+#     pass
+
+# class Electricity(db.Model):
+#     pass
+
+# class Health(db.Model):
+#     pass
+
+# class Security(db.Model):
+#     pass
+
+# class Others(db.Model):
+#     pass
